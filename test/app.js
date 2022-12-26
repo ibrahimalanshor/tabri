850c30bd4a641ab436b37b0377b6913929e8e610
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2022-12-27 04:23:59 +0700

    feat: app static

--- test/app.js
+++ test/app.js
@@ -3,30 +3,58 @@ const request = require('supertest');
 const tabri = require('../');
 
 describe('app', function () {
+  before(function () {
+    this.server = tabri({
+      port: 5000,
+      logging: false,
+      static: {
+        path: '/public',
+        dir: __dirname + '/static',
+      },
+    });
+
+    this.server.run(() => {});
+  });
+
+  after(function () {
+    this.server.stop();
+  });
+
   it('should be callable', function () {
     assert.equal(typeof tabri, 'function');
   });
 
   it('should return app', function () {
-    const server = tabri();
-
-    assert.equal(typeof server, 'object');
+    assert.equal(typeof this.server, 'object');
   });
 
   it('should return port from constructor config', function () {
-    const server = tabri({ port: 5000 });
-
-    assert.equal(server.app.get('port'), 5000);
+    assert.equal(this.server.app.get('port'), 5000);
   });
 
   it('should run app', function (done) {
-    const server = tabri({
-      port: 5000,
-      logging: false,
-    });
+    request('http://localhost:5000')
+      .get('/')
+      .expect(404)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
 
-    server.run(() => {});
+        done();
+      });
+  });
 
-    request('http://localhost:5000').get('/').expect(404, done);
+  it('should serves static files', function (done) {
+    request('http://localhost:5000')
+      .get('/public/test.txt')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        done();
+      });
   });
 });
